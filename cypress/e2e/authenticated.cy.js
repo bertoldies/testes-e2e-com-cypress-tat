d39a38545f5671b659ad8a9b94149f16999c4008
Jussara da Costa <jussara.costa@neogrid.com>
2024-01-24 15:00:34 -0300

    Fix lint issue

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -38,7 +38,6 @@ describe('Scenarios where authentication is a pre-condition', () => {
   it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
     cy.wait('@getNotes')
-    
     cy.contains('.nav a', 'Logout').click()
     cy.get('#email').should('be.visible')
   })
